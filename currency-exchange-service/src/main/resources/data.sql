4e1ef79cd678e079b4b24a09683eeec8be221ba9
Monica <m09ny@yahoo.com>
2019-02-22 14:12:46 +0200

    currency-exchange-service

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1001,'RON','EUR',25,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1002,'EUR','RON',48,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1003,'RON','USD',40,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port) values(1004,'USD','RON',30,0);
